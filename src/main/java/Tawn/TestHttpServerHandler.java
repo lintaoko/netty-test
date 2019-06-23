5ffff561aa58c625ecf0ade90879daeeca884e4e
lintaoko <1244275215@qq.com>
2019-06-23 15:01:49 +0800

    1

--- src/main/java/Tawn/TestHttpServerHandler.java
+++ src/main/java/Tawn/TestHttpServerHandler.java
@@ -7,20 +7,35 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
 
-        //构造Hello World 对象
-        ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
-        FullHttpResponse response =new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK,content);
+        if(msg instanceof HttpRequest){
 
-        response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain");
-        response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
+            //Http强制类型转换
+            HttpRequest httpRequest =(HttpRequest)msg;
 
+            System.out.println("请求方法名称:"+httpRequest.method().name());
 
-        //返回客户端
-        ctx.writeAndFlush(response);
+            URI uri=new URI(httpRequest.uri());
+            if ("/favicon.ico".equals(uri.getPath())){
+                System.out.println("请求favicon.ico");
+                return;
+            }
+            //构造Hello World 对象
+            ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
+            FullHttpResponse response =new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK,content);
+
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
+
+            //返回客户端
+            ctx.writeAndFlush(response);
+        }
+
     }
 }
